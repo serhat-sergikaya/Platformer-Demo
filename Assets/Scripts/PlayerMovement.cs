086c40c3a9c81d1995a4f1b1c51ce28771276ec1
Serhat Sergikaya <serhatsergikaya@gmail.com>
2024-08-03 17:14:18 +0300

    Set up input system

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMove(InputValue inputValue){
+        moveInput = inputValue.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
