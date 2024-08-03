43c135ca24dfdee07e709f8d0b7d8d5be2cb14ae
Serhat Sergikaya <serhatsergikaya@gmail.com>
2024-08-03 17:57:36 +0300

    Implement horizontal player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 2f;
     Vector2 moveInput;
+    Rigidbody2D playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     private void OnMove(InputValue inputValue){
         moveInput = inputValue.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    private void Run(){
+
+        Vector2 playerVelocity = new Vector2(moveInput.x* moveSpeed, playerRb.velocity.y);
+        playerRb.velocity = playerVelocity;
     }
 }
